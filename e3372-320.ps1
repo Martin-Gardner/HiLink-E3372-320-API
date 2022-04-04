bf78791462113f1338549a47a9d43da8f75480d4
Martin-Gardner <35814778+Martin-Gardner@users.noreply.github.com>
2022-04-04 11:04:25 +0100

    Update e3372-320.ps1
    Added missing API points - a few more to add like deleting SMS messages

--- e3372-320.ps1
+++ e3372-320.ps1
@@ -52,7 +52,39 @@ function New-SessionInfo
     $ConInfo["token"] = $token
     $ConInfo["SessionID"] = $key
     # Return the returnable variable
-    return $coninfo
+    return $ConInfo
+}
+
+function New-APIGetRequest
+{
+    param
+    (
+        $url,
+        $endpoint
+    )
+
+    $uri = "http://$url/$endpoint"
+
+    $ConInfo = new-SessionInfo $url
+
+     # Create HTTP header with token
+     $headers = @{
+        "__RequestVerificationToken" = $ConInfo["token"]
+    }
+
+    # Create a new cookie container and add the session ID
+    $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+    $cookie = New-Object System.Net.Cookie 
+    $cookie.Name = "SessionID"
+    $cookie.Value = $ConInfo["SessionID"]
+    $cookie.Domain = $ModemsIPAddress
+    $session.Cookies.Add($cookie)
+
+    # Create a new HTTP request object
+    [xml]$response = (Invoke-RestMethod -Uri $uri -Method Get -ContentType "text/xml" -Headers $headers -WebSession $session)
+
+    # Return the response
+    return $response
 }
 
 <#
@@ -93,6 +125,42 @@ function New-APIPostRequest
     return $response
 }
 
+Function Clear-DeviceTrafficStats
+{
+    param(
+        $ModemIpAddress # Ip address of the HiLink E3372
+    )
+
+    $API_ClearTrafficStats = "api/monitoring/clear-traffic"
+    $data = "<request><ClearTraffic>1</ClearTraffic></request>"
+    [xml]$response = (New-APIPostRequest -url $ModemsIPAddress -endpoint $API_ClearTrafficStats -data $data)
+    return $response
+}
+function Set-DeviceInternetOnline
+{
+    param(
+        $ModemIpAddress # Ip address of the HiLink E3372
+    )
+
+    $API_DeviceInternetOnline = "api/dialup/mobile-dataswitch"
+    $data = "<request><dataswitch>1</dataswitch></request>"
+    [xml]$response = (New-APIPostRequest -url $ModemsIPAddress -endpoint $API_DeviceInternetOnline -data $data)
+    return $response
+}
+
+function Set-DeviceInternetOffline
+{
+    param(
+        $ModemIpAddress # Ip address of the HiLink E3372
+    )
+
+    $API_DeviceInternetOffline = "api/dialup/mobile-dataswitch"
+    $data = "<request><dataswitch>0</dataswitch></request>"
+    [xml]$response = (New-APIPostRequest -url $ModemsIPAddress -endpoint $API_DeviceInternetOffline -data $data)
+    return $response
+}
+
+
 # Get a list of SMS messages from e3372-320 modem
 Function Get-SMSMessages {
     
@@ -108,7 +176,8 @@ Function Get-SMSMessages {
     $data = "<request><PageIndex>1</PageIndex><ReadCount>20</ReadCount><BoxType>1</BoxType><SortType>0</SortType><Ascending>0</Ascending><UnreadPreferred>1</UnreadPreferred></request>"
 
     # return xml response
-    return (New-APIPostRequest -url $ModemsIPAddress -endpoint $API_SMSList -data $data)
+    [xml]$response = (New-APIPostRequest -url $ModemIpAddress -endpoint $API_SMSList -data $data)
+    return $response
 }
 
 # Send a SMS message to a phone number
@@ -126,7 +195,71 @@ Function New-SMSMessage {
 
     # Data to be sent to the API call
     $data = "<?xml version='1.0' encoding='UTF-8'?><request><Index>-1</Index><Phones><Phone>$PhoneNumber</Phone></Phones><Sca></Sca><Content>$Message</Content><Length>-1</Length><Reserved>1</Reserved><Date>-1</Date></request>"
-    return $(New-APIPostRequest -url $ModemsIPAddress -endpoint $API_SendSMS -data $data)
+    [xml]$response = (New-APIPostRequest -url $ModemsIPAddress -endpoint $API_SendSMS -data $data)
+    return $response
+}
+
+function Get-DeviceInformation
+{
+
+    param(
+        $ModemIpAddress # Ip address of the HiLink E3372
+    )
+
+    $API_DeviceInformation = "api/device/information"
+
+    [xml]$response = (New-APIGetRequest -url $ModemsIPAddress -endpoint $API_DeviceInformation)
+    return $response
+
+}
+
+function  Get-DeviceStatus 
+{
+
+    param(
+        $ModemIpAddress # Ip address of the HiLink E3372
+    )
+
+    $API_DeviceStatus = "api/monitoring/status"
+
+    [xml]$response = (New-APIGetRequest -url $ModemsIPAddress -endpoint $API_DeviceStatus)
+    return $response
+}
+
+function Get-DeviceNotifications
+{
+    param(
+        $ModemIpAddress # Ip address of the HiLink E3372
+    )
+
+    $API_DeviceNotifications = "api/monitoring/check-notifications"
+
+    [xml]$response = (New-APIGetRequest -url $ModemsIPAddress -endpoint $API_DeviceNotifications)
+    return $response
+}
+
+function Get-DeviceNetworkInfo 
+{
+    param(
+        $ModemIpAddress # Ip address of the HiLink E3372
+    )
+    
+    $API_DeviceNetworkInfo = "api/net/current-plmn"
+
+    [xml]$response = (New-APIGetRequest -url $ModemsIPAddress -endpoint $API_DeviceNetworkInfo)
+    return $response
+}
+
+function Get-DeviceTrafficStats
+{
+    param(
+        $ModemIpAddress # Ip address of the HiLink E3372
+    )
+
+    $API_DeviceTrafficStats = "api/monitoring/traffic-statistics"
+
+    [xml]$response = (New-APIGetRequest -url $ModemsIPAddress -endpoint $API_DeviceTrafficStats)
+    return $response
 }
 
 # Get Stored SMS Messages
@@ -134,7 +267,58 @@ Function New-SMSMessage {
 #$output = Get-SMSMessages -ModemIpAddress $ModemsIPAddress
 #$output.innerXML
 
+#########################
+
 # Send a SMS message to a phone number
 
 #$output = New-SMSMessage -ModemIpAddress $ModemsIPAddress -PhoneNumber "0123456789" -Message "Hello World"
+#$output.innerXML
+
+#########################
+
+# Get Device Information
+
+#$output = Get-DeviceInformation -ModemIpAddress $ModemsIPAddress
+#$output.innerXML
+
+#########################
+
+# Get Device Status
+
+#$output = Get-DeviceStatus -ModemIpAddress $ModemsIPAddress
+#$output.innerXML
+
+#########################
+
+# Get Device Notification
+
+#$output = Get-DeviceNotifications -ModemIpAddress $ModemsIPAddress
+#$output.innerXML
+
+#########################
+
+# Get Device Network Information
+
+#$output = Get-DeviceNetworkInfo -ModemIpAddress $ModemsIPAddress
+#$output.innerXML
+
+#########################
+
+# Set Device Internet Offline
+
+#$output = Set-DeviceInternetOffline -ModemIpAddress $ModemsIPAddress
+#$output.innerXML
+
+#########################
+
+# Set Device Internet Online
+
+#$output = Set-DeviceInternetOnline -ModemIpAddress $ModemsIPAddress
+#$output.innerXML
+
+#########################
+
+# Get Device Traffic Statistics
+
+#$output = Get-DeviceTrafficStats -ModemIpAddress $ModemsIPAddress
 #$output.innerXML
